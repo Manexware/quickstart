e1d6c252cb0e751ab02c1250f55b83040c0937e2
Manuel Vega Ulloa <mevu@pop-os.localdomain>
2020-08-11 13:50:19 -0500

    first commit

---
+++ modules/core/build/db/60-session-planner/init/hsql/10.create-db.sql
@@ -0,0 +1,76 @@
+-- begin SESSIONPLANNER_SPEAKER
+create table SESSIONPLANNER_SPEAKER (
+    ID varchar(36) not null,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    FIRST_NAME varchar(255) not null,
+    LAST_NAME varchar(255),
+    EMAIL varchar(255) not null,
+    --
+    primary key (ID)
+)^
+-- end SESSIONPLANNER_SPEAKER
+-- begin SESSIONPLANNER_SESSION
+create table SESSIONPLANNER_SESSION (
+    ID varchar(36) not null,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    TOPIC varchar(255) not null,
+    START_DATE timestamp not null,
+    DURATION integer not null,
+    SPEAKER_ID varchar(36) not null,
+    DESCRIPTION longvarchar,
+    --
+    primary key (ID)
+)^
+-- end SESSIONPLANNER_SESSION
+-- begin PLANNER_SPEAKER
+create table PLANNER_SPEAKER (
+    ID varchar(36) not null,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    FIRST_NAME varchar(255) not null,
+    LAST_NAME varchar(255),
+    EMAIL varchar(255) not null,
+    --
+    primary key (ID)
+)^
+-- end PLANNER_SPEAKER
+-- begin PLANNER_SESSION
+create table PLANNER_SESSION (
+    ID varchar(36) not null,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    TOPIC varchar(255) not null,
+    START_DATE timestamp not null,
+    DURATION integer not null,
+    SPEAKER_ID varchar(36) not null,
+    DESCRIPTION longvarchar,
+    --
+    primary key (ID)
+)^
+-- end PLANNER_SESSION
